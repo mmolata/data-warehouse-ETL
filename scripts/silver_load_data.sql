89e27d94a5b25b29e0540f920859b3c0dc58f100
ipaapaa <marc.olata123@gmail.com>
2025-09-28 14:08:32 +0800

    add load data script to silver.crm_cust_info

---
+++ scripts/silver_load_data.sql
@@ -0,0 +1,32 @@
+-- Insert cleaned + deduped data into silver layer
+INSERT INTO silver.crm_cust_info (
+    cst_id,
+    cst_key,
+    cst_firstname,
+    cst_lastname,
+    cst_marital_status,
+    cst_gndr,
+    cst_create_date
+)
+SELECT
+    cst_id,
+    TRIM(cst_key) AS cst_key,
+    TRIM(cst_firstname) AS cst_firstname,
+    TRIM(cst_lastname) AS cst_lastname,
+    CASE 
+        WHEN UPPER(TRIM(cst_marital_status)) = 'S' THEN 'Single'
+        WHEN UPPER(TRIM(cst_marital_status)) = 'M' THEN 'Married'
+        ELSE 'Unknown'
+    END AS cst_marital_status,
+    CASE 
+        WHEN UPPER(TRIM(cst_gndr)) = 'F' THEN 'Female'
+        WHEN UPPER(TRIM(cst_gndr)) = 'M' THEN 'Male'
+        ELSE 'Unknown'
+    END AS cst_gndr,
+    cst_create_date
+FROM (
+    SELECT *,
+           RANK() OVER (PARTITION BY cst_id ORDER BY cst_create_date DESC) AS flag_recent
+    FROM bronze.crm_cust_info
+) t
+WHERE flag_recent = 1;
