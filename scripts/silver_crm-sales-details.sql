f940ba89f3cdd933658821c1e92afdfc71f735f9
ipaapaa <marc.olata123@gmail.com>
2025-09-29 07:25:36 +0800

    cleaned columns sls_ship_dt and sls_due_dt

--- scripts/silver_crm-sales-details.sql
+++ scripts/silver_crm-sales-details.sql
@@ -8,8 +8,20 @@ CASE WHEN sls_order_dt = 0
 	THEN NULL
 	ELSE CAST(CAST(sls_order_dt AS VARCHAR) AS DATE)
 END as sls_order_dt,
-sls_ship_dt,
-sls_due_dt,
+CASE WHEN sls_ship_dt = 0 
+	OR LENGTH(sls_ship_dt::text) != 8
+	OR sls_ship_dt > 20501230
+	OR sls_ship_dt < 19500101
+	THEN NULL
+	ELSE CAST(CAST(sls_ship_dt AS VARCHAR) AS DATE)
+END as sls_ship_dt,
+CASE WHEN sls_due_dt = 0 
+	OR LENGTH(sls_due_dt::text) != 8
+	OR sls_due_dt > 20501230
+	OR sls_due_dt < 19500101
+	THEN NULL
+	ELSE CAST(CAST(sls_due_dt AS VARCHAR) AS DATE)
+END as sls_due_dt,
 sls_sales,
 sls_quantity,
 sls_price
